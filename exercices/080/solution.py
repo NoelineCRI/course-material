2c65f877453824d906701074907d93a354043f1d
Noëline LEVI ALVARES <noeline.levialvares@cri-paris.org>
2014-09-22 15:27:10 +0200

    solut 070

---
+++ exercices/080/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+a = "abcdefghijklmnopqrstuvwxyz"
+for l in a:
+    for l2 in a: 
+        if l2>l:
+            print(l+l2)
