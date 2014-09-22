cf997094647cfb13513ead384cfce3538f8890da
Noëline LEVI ALVARES <noeline.levialvares@cri-paris.org>
2014-09-22 14:30:04 +0200

    solut 013

---
+++ exercices/013/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+import sys
+if len(sys.argv)>1:
+    print(sys.argv[1])
+else:
+    print("usage: python",sys.argv[0],"PARAM")
