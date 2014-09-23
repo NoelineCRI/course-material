1e48441d45bac17ef4878a8adc68318c9c38cf54
Noeline LEVI ALVARES <noeline.levialvares@CRI-paris.org>
2014-09-23 17:11:02 +0200

    solut 220

--- exercices/220/solution.py
+++ exercices/220/solution.py
@@ -8,13 +8,22 @@ Created on Mon Sep 22 18:16:14 2014
 # Print every prime numbers in a range
 
 
-import is_primeDef
+def is_prime(n):
+    prime = True
+    for i in range(2, n):
+        if (n % i) == 0:
+            prime = False
+            # print(n,"est divisible par",i)
+            break
+        # else:
+            # print(n,"n'est pas divisible par",i)
+    return(prime)
 
 
 def first_prime(a, b):  # Return the 1st prime of the range [a,b]
     p = 0
     for i in range(a, b):
-        if is_primeDef.is_prime(i):
+        if is_prime(i):
             p = i
             break
     return(p)
@@ -22,7 +31,7 @@ def first_prime(a, b):  # Return the 1st prime of the range [a,b]
 l_prime = ""
 l_prime += str(first_prime(10000, 10050))
 for j in range(first_prime(10000, 10050) + 1, 10050):
-    if is_primeDef.is_prime(j):
+    if is_prime(j):
         l_prime += ", "
         l_prime += str(j)
 print(l_prime)
