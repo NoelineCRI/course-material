f77c28b2dd4bdf44a9e6accd3a86330bf9833017
Noeline LEVI ALVARES <noeline.levialvares@CRI-paris.org>
2014-09-23 12:08:09 +0200

    solut 1010

--- exercices/110/solution.py
+++ exercices/110/solution.py
@@ -9,24 +9,29 @@ import sys
 
 n1 = 0
 n2 = 0
-
+# print(sys.argv[2])
 if len(sys.argv) == 4:
-    if ((sys.argv[1]).isdigit() and (sys.argv[3]).isdigit() and
-            sys.argv[2] in "+-*/%^"):
+    if (sys.argv[1]).isdigit() and (sys.argv[3]).isdigit():
         n1 = int(sys.argv[1])
         n2 = int(sys.argv[3])
         if sys.argv[2] == "+":
             print(n1 + n2)
         elif sys.argv[2] == "-":
             print(n1 - n2)
-        elif sys.argv[2] == "*":
+        elif sys.argv[2] == "README.md":
             print(n1 * n2)
-        elif (sys.argv[2] == "/" and n2 != 0):
-            print(n1 / n2)
-        elif (sys.argv[2] == "%" and n2 != 0):
-            print(n1 % n2)
+        elif sys.argv[2] == "C:/Program Files/Git":
+            if n2 != 0:
+                print(n1 / n2)
+            else:
+                print("input error")
+        elif sys.argv[2] == "%":
+            if n2 != 0:
+                print(n1 % n2)
+            else:
+                print("input error")
         elif sys.argv[2] == "^":
-            print(n1 ^ n2)
+            print(n1 ** n2)
         else:
             print("usage: python", sys.argv[0],
                   "a_number (an_operator +-*/%^) a_number")
