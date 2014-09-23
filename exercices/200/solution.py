24ea96c3810f3e154727fdcd62c0d01c19d616ad
Noeline LEVI ALVARES <noeline.levialvares@CRI-paris.org>
2014-09-23 12:12:23 +0200

    solut 200

--- exercices/200/solution.py
+++ exercices/200/solution.py
@@ -5,23 +5,21 @@ Created on Mon Sep 22 18:16:14 2014
 @author: nono
 """
 
-#heck if its given number is a prime one
+# Check if its given number is a prime one
+
 
 def is_prime(n):
     prime = True
-    for i in range(2,n):
-        if (n%i)== 0 :
-            prime= False
-            #print(n,"est divisible par",i)
+    for i in range(2, n):
+        if (n % i) == 0:
+            prime = False
+            # print(n,"est divisible par",i)
             break
-        #else:
-            #print(n,"n'est pas divisible par",i)
+        # else:
+            # print(n,"n'est pas divisible par",i)
     return(prime)
-    
-prime = is_prime(15)
 
-#if prime:
-#    print("It is a prime number!")
-#else:
-#    print("It isn't a prime number")
-    
+# if prime:
+#     print("It is a prime number!")
+# else:
+#     print("It isn't a prime number")
