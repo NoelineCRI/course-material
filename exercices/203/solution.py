116c38cc382f041cc1fcc3497714f56db009e038
Noeline LEVI ALVARES <noeline.levialvares@cri-paris.org>
2014-09-25 11:43:06 +0200

    solut 203

---
+++ exercices/203/solution.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# is_multiple_of
+# return true if a is a multiple of b
+
+
+def is_multiple_of(a, b):  # a, b numbers
+    return((a % b) == 0)
+
+# print(is_multiple_of(16, 4))
